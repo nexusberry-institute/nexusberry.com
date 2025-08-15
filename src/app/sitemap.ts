2eb7ecd4f56a5819461f7644e37add3cf89f8366
nexusberry-institute <nexusberry.it@gmail.com>
2025-08-15 13:20:40 +0500

    ++ pnpm up

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -192,6 +192,12 @@ export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
       changeFrequency: 'weekly',
       priority: 0.8,
     },
+    {
+      url: 'https://www.nexusberry.com/forms',
+      lastModified: new Date(),
+      changeFrequency: 'weekly',
+      priority: 0.8,
+    },
     // about-us
     // gallery
     // training-schedule
