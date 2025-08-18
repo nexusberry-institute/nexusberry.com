0cf446348260df7198df6a2ba587ac4d0d3373b7
nexusberry-institute <nexusberry.it@gmail.com>
2025-08-18 10:24:26 +0500

    Update layout.tsx

--- src/app/(frontend)/layout.tsx
+++ src/app/(frontend)/layout.tsx
@@ -57,7 +57,7 @@ export default async function RootLayout({ children }: { children: React.ReactNo
           <img
             height="1"
             width="1"
-            style="display:none"
+            style={{ display: "none" }}
             src="https://www.facebook.com/tr?id=1454534582531274&ev=PageView&noscript=1" />
         </noscript>
         {/* Meta Pixel Code - end */}
