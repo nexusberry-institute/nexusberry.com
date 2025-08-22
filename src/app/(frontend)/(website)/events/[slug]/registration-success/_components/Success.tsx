d185c2e97dafa7371452b6d06a21fac4efdd01c9
Jehanzaib084 <muhammadjehanzaib2021@gmail.com>
2025-08-22 13:49:00 +0000

    Enhance loading spinner styles and improve layout in RetriveUserRegistration component; update Success component background styling

--- src/app/(frontend)/(website)/events/[slug]/registration-success/_components/Success.tsx
+++ src/app/(frontend)/(website)/events/[slug]/registration-success/_components/Success.tsx
@@ -17,8 +17,14 @@ export default function Success({ slug }: { slug: string }) {
   }, [])
 
   return (
-    <div className="flex text-card flex-col  pt-16  items-center mx-auto h-[25rem] space-y-6 p-4 bg-primary-400 ">
-      <div className="bg-green-500 rounded-full h-fit  animate-ping w-fit p-4  ">
+    <div
+      className="flex text-card flex-col pt-16 items-center mx-auto h-[25rem] space-y-6 p-4"
+      style={{
+        background: `url(https://www.wscubetech.com/images/master-class/master-banner-checks.svg) no-repeat 50%, url(https://www.wscubetech.com/images/categories/noise.png) repeat, linear-gradient(256.99deg, #002ccd 19.72%, #003238ff 97.05%), linear-gradient(180deg, #0834d8, rgba(25, 76, 255, 0))`,
+        backgroundBlendMode: 'color-dodge',
+      }}
+    >
+      <div className="bg-green-500 rounded-full h-fit animate-ping w-fit p-4">
         <Check strokeWidth={4} size={30} className="stroke-card" />
       </div>
       <h1 className="text-2xl font-bold">Hello , {applicant.name}</h1>
