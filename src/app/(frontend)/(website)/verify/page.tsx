508218a73b264c1cb7a1014575392682b3b91c0c
nexusberry-institute <nexusberry.it@gmail.com>
2025-08-24 00:44:12 +0500

    verify page created

---
+++ src/app/(frontend)/(website)/verify/page.tsx
@@ -0,0 +1,14 @@
+export default function VerifyPage() {
+  return (
+    <div className="h-screen flex flex-col justify-center items-center space-y-4">
+      <h1 className="text-3xl">Following Verification System - Coming Soon</h1>
+      <div>Verify Course Certificate</div>
+      <div>Verify Event Certificate</div>
+      <div>Verify Internship Letter</div>
+      <div>Verify Job Expereince Letter</div>
+      <div>Verify Student</div>
+      <div>Verify Staff</div>
+      <div>Verify Faculty</div>
+    </div>
+  )
+}
