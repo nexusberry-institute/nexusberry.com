1093f872441a6dfbebe1ad30c5b164b17c42a56a
nexusberry-institute <nexusberry.it@gmail.com>
2025-08-25 01:43:09 +0500

    ++ Google Tag Manager Script

--- src/app/(frontend)/(website)/layout.tsx
+++ src/app/(frontend)/(website)/layout.tsx
@@ -46,6 +46,29 @@ export default async function Layout({ children }: { children: React.ReactNode }
       />
       {/* End Google Analytics - Google tag (gtag.js) */}
 
+      {/* Start Google Tag Manager*/}
+      <Script
+        id="gtm-head"
+        strategy="afterInteractive"
+        dangerouslySetInnerHTML={{
+          __html: `
+              (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+              new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+              j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+              'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+              })(window,document,'script','dataLayer','GTM-NRCLDWRV');
+            `,
+        }}
+      />
+      <noscript>
+        <iframe
+          src="https://www.googletagmanager.com/ns.html?id=GTM-NRCLDWRV"
+          height="0"
+          width="0"
+          style={{ display: "none", visibility: "hidden" }}
+        />
+      </noscript>
+      {/* End Google Tag Manager*/}
 
       <Header />
       {children}
