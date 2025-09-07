8ddab2950ed0b8d740a09057975bbe376162830a
nexusberry-institute <nexusberry.it@gmail.com>
2025-09-07 23:27:14 +0500

    Update revalidatePost.ts

--- src/collections/Posts/hooks/revalidatePost.ts
+++ src/collections/Posts/hooks/revalidatePost.ts
@@ -1,7 +1,5 @@
 import type { CollectionAfterChangeHook, CollectionAfterDeleteHook } from 'payload'
-
-import { revalidatePath, revalidateTag } from 'next/cache'
-
+import { revalidatePath } from 'next/cache'
 import type { Post } from '../../../payload-types'
 
 export const revalidatePost: CollectionAfterChangeHook<Post> = ({
@@ -11,19 +9,14 @@ export const revalidatePost: CollectionAfterChangeHook<Post> = ({
 }) => {
   if (!context.disableRevalidate) {
     if (doc._status === 'published') {
-      const path = `/posts/${doc.slug}`
-
-      payload.logger.info(`Revalidating post at path: ${path}`)
-
+      const path = `/blog/${doc.slug}`
+      payload.logger.info(`Revalidating  detail: ${path}`)
       revalidatePath(path)
     }
-
-    // If the post was previously published, we need to revalidate the old path
+    // If the blog was previously published, we need to revalidate the old path
     if (previousDoc._status === 'published' && doc._status !== 'published') {
-      const oldPath = `/posts/${previousDoc.slug}`
-
-      payload.logger.info(`Revalidating old post at path: ${oldPath}`)
-
+      const oldPath = `/blog/${previousDoc.slug}`
+      payload.logger.info(`Revalidating old blog detail: ${oldPath}`)
       revalidatePath(oldPath)
     }
   }
@@ -32,10 +25,8 @@ export const revalidatePost: CollectionAfterChangeHook<Post> = ({
 
 export const revalidateDelete: CollectionAfterDeleteHook<Post> = ({ doc, req: { context } }) => {
   if (!context.disableRevalidate) {
-    const path = `/posts/${doc?.slug}`
-
+    const path = `/blog/${doc?.slug}`
     revalidatePath(path)
   }
-
   return doc
 }
