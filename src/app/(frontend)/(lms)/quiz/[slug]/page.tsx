6f5bfc087287d7943b3ed8052e6210e115c73773
neusberry-classroom <nexusberry.solution@gmail.com>
2025-12-02 00:26:32 +0500

    quiz page updated

---
+++ src/app/(frontend)/(lms)/quiz/[slug]/page.tsx
@@ -0,0 +1,10 @@
+
+export default function QuizBySlugPage() {
+  return (
+    <div className="min-h-screen bg-gray-100 py-12 px-4 sm:px-6 lg:px-8">
+      <div className="max-w-lg mx-auto">
+        <h1 className="text-3xl font-bold text-center mb-8"> Fetch Course By Slug </h1>
+      </div>
+    </div>
+  );
+}
--- src/app/(frontend)/(lms)/quiz/page.tsx
+++ src/app/(frontend)/(lms)/quiz/page.tsx
@@ -4,9 +4,30 @@ export default function QuizPage() {
   return (
     <div className="min-h-screen bg-gray-100 py-12 px-4 sm:px-6 lg:px-8">
       <div className="max-w-lg mx-auto">
-        <h1 className="text-3xl font-bold text-center mb-8">Python Programming Quiz: Functions</h1>
+        <h1 className="text-3xl font-bold text-center mb-8">Quiz: Python Functions</h1>
         <Quiz />
       </div>
     </div>
   );
 }
+
+// export default function QuizPage() {
+
+    // Fetch and Render quiz by Module or Module topic via search params 
+    // <ModuleQuiz />
+
+//   if no search params  
+//   return (
+//     <div className="min-h-screen bg-gray-100 py-12 px-4 sm:px-6 lg:px-8">
+//       <div className="max-w-lg mx-auto">
+//         <h1 className="text-3xl font-bold text-center mb-8"> Select: </h1>
+//         <select className="text-3xl font-bold text-center mb-8"> Subject: Fecth Modules </select>
+//         <select className="text-3xl font-bold text-center mb-8"> Topics: Fecth Module Topic </select>
+//         <h1 className="text-3xl font-bold text-center mb-8">
+//           Browse Pre-made QUIZZES : Goto Listing Page
+//         </h1>
+//         {/* <QuizzesListing /> */}
+//       </div>
+//     </div>
+//   );
+// }
