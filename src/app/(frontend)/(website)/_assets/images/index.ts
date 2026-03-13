f50b40b0ad720fabb482e81b67cbd7d8f0260acd
NexusBerry Classroom <nexusberry.it@gmail.com>
2026-03-13 17:40:44 +0500

    deleted unused images

--- src/app/(frontend)/(website)/_assets/images/index.ts
+++ src/app/(frontend)/(website)/_assets/images/index.ts
@@ -6,7 +6,6 @@ import dataAnalytics from './dataAnalytics.png'
 import Django from './Django.jpg'
 import dl from './dl.jpg'
 import Dsml from './Dsml.jpg'
-import flutter2 from './flutter2.png'
 import graphic from './graphic_design.jpg'
 import javascript from './javascript.jpg'
 import mean from './mean.jpg'
@@ -23,26 +22,16 @@ import unity_game from './unity_game.jpg'
 import webdesigning from './webdesigning.jpg'
 import wordpress from './wordpress.jpg'
 import power_bi from './Power_bi.jpg'
-import RanaAjmal from './Rana-Ajmal.png'
-import thambnail from './thambnail.jpg'
 import HeroBgTmg from './herobackImg.webp'
 import beginer from './beginer.svg'
 import heroCourse2 from './heroCourse2.webp'
 import bgImg from './bg-img.webp'
 
 import eventsBanner from './eventBanner.svg'
-import master3 from './master3.webp'
-import masterNew from './masterNew.webp'
-import masterclass from './masterclass.webp'
-import scan from './masterclass-whatsapp-channel.svg'
 import learning from './learning.webp'
 import Certificate from './certificate.svg'
-import mentor from './mentor.png'
-import mentor2 from './mentor2.png'
 import ourMentor from './our-mentor.svg'
-import dataVisual from './dataVisualisation.webp'
 import whtsppIcon from './whatsapp-icon.svg'
-import Majid from './Majid.jpg'
 import Map from './map.jpg'
 
 import twitter from './twitter.svg'
@@ -50,38 +39,24 @@ import youtube from './youtub.svg'
 import facebook from './facebook.svg'
 import instagram from './insta.svg'
 import modulebg from './learning-bg.svg'
-import module1 from './module1.svg'
-import module2 from './module2.svg'
-import module3 from './module3.svg'
-import module4 from './module4.svg'
-import module5 from './module5.svg'
-import module6 from './module6.svg'
 import placeholderImg from './placeholderImg.jpg'
-import login_hero from './login-hero.svg'
-import certification from './certificate-icon.svg'
 
 import interactiveLearning from './1.png'
 import extraLearn from './2.png'
 import Quz from './3.png'
 import certificate from './4.png'
-import RegisterForFreeImg from './RegisterForFreeImg.jpg'
 import register from './register.png'
 import nexusBerryImg from './photo2.webp'
 
 export {
   android, flutter, Asp, dataAnalytics, Django, dl, Dsml,
-  flutter2, graphic, javascript, mean, Mern,
+  graphic, javascript, mean, Mern,
   nextjs, PhpLaravel, python, react, reactNative, seo,
   social_media, social, unity_game, webdesigning, wordpress, power_bi,
-  RanaAjmal, thambnail, HeroBgTmg, beginer,
-  eventsBanner, master3, masterNew, masterclass,
-  scan, learning, Certificate, mentor, mentor2,
-  ourMentor, dataVisual, whtsppIcon, register, nexusBerryImg,
+  HeroBgTmg, beginer,
+  eventsBanner, learning, Certificate,
+  ourMentor, whtsppIcon, register, nexusBerryImg,
   Map, twitter, facebook, instagram, youtube,
-  heroCourse2, bgImg, Majid, certification, interactiveLearning, extraLearn, Quz, certificate,
-  module1, module2, module3, module4, module5, module6, modulebg, placeholderImg, login_hero, RegisterForFreeImg
+  heroCourse2, bgImg, interactiveLearning, extraLearn, Quz, certificate,
+  modulebg, placeholderImg
 }
-
-
-
-
